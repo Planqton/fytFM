3d19891f3189e4c2e5d45a2638c53dbbce2b3263
planqton <planqton@users.noreply.github.com>
2026-02-25 22:39:45 +0100

    Release v1.1.3
    - versionName: 1.1.3
    - versionCode: 10103

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "at.planqton.fytfm"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10102
-        versionName = "1.1.2"
+        versionCode = 10103
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
