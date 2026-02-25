27331721eda2e216b1ccdebd5565695108c54aaf
planqton <planqton@users.noreply.github.com>
2026-02-25 22:32:38 +0100

    Release v1.1.2
    - versionName: 1.1.2
    - versionCode: 10102

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "at.planqton.fytfm"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10101
-        versionName = "1.1.1"
+        versionCode = 10102
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
